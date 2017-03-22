117d3ce8ce6315927b4d468b5c81fafdacda1347
Kayaya <1kayat34@solent.ac.uk>
2017-03-22 14:59:36 +0000

    NetworkCommunication - Finished exercices 1,2,3...

--- app/src/main/java/com/example/a1kayat34/networkcommunication/MainActivity.java
+++ app/src/main/java/com/example/a1kayat34/networkcommunication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a1kayat34.networkcommunication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -16,46 +17,46 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    class MyTask extends AsyncTask<Void, Void, String>{
-        public String doInBackground(Void... unused){
+    class SongAsyncTask extends AsyncTask<String, Void, String> {
+        private static final int RESPONSE_OK = 200;
+
+        @Override
+        protected String doInBackground(String... strings) {
             HttpURLConnection conn = null;
-            try{
-                URL url = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php?artist=Oasis");
+            try {
+                //Before the change
+                //URL url = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php?artist=" + strings[0]);
+                URL url = new URL(strings[0]+ "?artist="+strings[1]);
                 conn = (HttpURLConnection) url.openConnection();
 
-                //EditText = (EditText) findViewById(R.id.search);
 
                 InputStream in = conn.getInputStream();
-                if(conn.getResponseCode() == 200)
-                {
+                //if connection response code OK
+                if (conn.getResponseCode() == RESPONSE_OK) {
+                    //start reading into string
                     BufferedReader br = new BufferedReader(new InputStreamReader(in));
-                    String result = "", line;
-                    while((line = br.readLine()) !=null)
-                        result += line;
+                    String result = null;
+                    String Currentline = null;
+                    while ((Currentline = br.readLine()) != null)
+                        result += Currentline;
                     return result;
-                }
-                else
+                } else
                     return "HTTP ERROR: " + conn.getResponseCode();
 
-            }
-            catch (IOException e){
-                return e.toString();
-            }
-            finally
-            {
-                if(conn!=null)
-                    conn.disconnect();
+
+            } catch (IOException e) {
+                return "Error" + e.toString();
             }
 
+            //return null;
         }
-        public void onPostExecute(String result)
-        {
-            TextView et1 = (TextView) findViewById(R.id.results);
+        @Override
+        public void onPostExecute(String result) {
+            TextView et1 = (TextView)findViewById(R.id.results);
             et1.setText(result);
         }
-
     }
 
     @Override
@@ -67,11 +68,19 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
 
-
-
     @Override
     public void onClick(View view) {
-        MyTask t = new MyTask();
-        t.execute();
+        /*MyTask t = new MyTask();
+        t.execute();*/
+
+        EditText urlET = (EditText)findViewById(R.id.url);
+
+        String url = urlET.getText().toString();
+
+        EditText artistEditText = (EditText) findViewById(R.id.search);
+        String artist = artistEditText.getText().toString();
+
+        SongAsyncTask task = new SongAsyncTask();
+        task.execute(url, artist);
     }
 }
---
+++ app/src/main/res/SongAsyncTask.java
@@ -0,0 +1,8 @@
+import android.os.AsyncTask;
+
+/**
+ * Created by 1kayat34 on 22/03/2017.
+ */
+public class SongAsyncTask extends AsyncTask<String, Void, String> {
+
+}
